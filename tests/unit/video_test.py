67c9ab8b715c90b30102b1153c7a246ac6585850
Thomas Margnac <t.margnac@gmail.com>
2024-09-13 19:43:53 +0200

    adding tests

--- tests/unit/video_test.py
+++ tests/unit/video_test.py
@@ -53,3 +53,40 @@ def test_video_metadata_without_ffprobe(monkeypatch):
         "ffprobe error (see stderr output for detail)"
     )
     assert str(error.value) == expected_error
+
+def test_video_cut():
+    video = Video(test_files[0])
+    video.clip(
+        start=0,
+        end=10
+    )
+    output = video.metadata()
+    assert output["format_name"] == "mov,mp4,m4a,3gp,3g2,mj2"
+    assert int(float(output["duration"])) == 10
+    assert len(output["streams"]) == 2
+    assert output["streams"][0]["codec_name"] == "h264"
+    assert output["streams"][1]["codec_name"] == "aac"
+
+def test_video_cut_without_ffmpeg(monkeypatch):
+    # Mocking FFmpeg not installed
+    def mock_ffmpeg(*args, **kwargs):
+        raise ffmpeg.Error(
+            "ffmpeg",
+            "stdout",
+            "stderr"
+        )
+
+    # Replace ffmpeg.run by mocking
+    monkeypatch.setattr(ffmpeg, "run", mock_ffmpeg)
+
+    # Testing
+    video = Video(test_files[0])
+    with pytest.raises(ffmpeg.Error) as error:
+        video.clip(
+            start=0,
+            end=10
+        )
+    expected_error = (
+        "ffmpeg error (see stderr output for detail)"
+    )
+    assert str(error.value) == expected_error
