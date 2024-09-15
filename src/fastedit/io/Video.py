1e71793f5e9b9e8c49a060ab838d2a38c261356b
Thomas Margnac <t.margnac@gmail.com>
2024-09-15 15:07:46 +0200

    fixing docstring

--- src/fastedit/io/Video.py
+++ src/fastedit/io/Video.py
@@ -7,10 +7,26 @@ class Video(_Media):
         self,
         path: str
     ):
+        """
+        Initializes an instance of video with the specified path.
+
+        Parameters
+        ----------
+        path: str
+            Path to the video file.
+
+        Raises
+        ------
+        TypeError
+            If the specified path is not a str.
+        """
+        # Guess mime type
         file_mime_type = _guess_file_type(path)
+        # Verifying that mime type is video
         if not file_mime_type == "video":
             raise TypeError(
                 f"Invalid file type: Expected a video file, got "
                 f"{file_mime_type} file instead."
             )
+        # Initialize instance
         super().__init__(path)
